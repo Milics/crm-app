5770de7d926d2c4bebee68fe529587d1861d32a7
Milics <milics@users.noreply.github.com>
2026-08-30 10:01:52 +0800

    fix: set base-href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4059083670",
+    serviceWorkerVersion: "1976946248",
   },
   config: {
     canvasKitBaseUrl: "/canvaskit/",
