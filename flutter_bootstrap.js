dd2617c61a79d13fcb1de991d325c27d734b59b0
Milics <milics@users.noreply.github.com>
2026-09-03 02:21:32 +0800

    fix: 修复日期解析FormatException

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2280993305",
+    serviceWorkerVersion: "3750453019",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
