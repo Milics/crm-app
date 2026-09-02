a2e7982856caf660fb89ab60fe5e974aa223d90c
Milics <milics@users.noreply.github.com>
2026-09-03 01:58:00 +0800

    fix: Web端双向智能同步上报与Mixed Content防护

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4018464815",
+    serviceWorkerVersion: "1053301735",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
