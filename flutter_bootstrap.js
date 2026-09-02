782a432c742f44df2b5eda91222db34fc3e26296
Milics <milics@users.noreply.github.com>
2026-09-03 02:15:44 +0800

    fix: 默认直连云端并将网络超时放宽至10秒

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2233579626",
+    serviceWorkerVersion: "2280993305",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
