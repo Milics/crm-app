52e31057724737a5a83016d6d976970ff6078a7e
Milics <milics@users.noreply.github.com>
2026-09-03 17:12:25 +0800

    feat: 发布支持真实图片物料+聊天截图+大模型诊断的Web版

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3750453019",
+    serviceWorkerVersion: "413824701",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
