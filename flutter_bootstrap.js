ef1e1ed71fdfd1fdb23b466aee379c5e8ca12749
Milics <milics@users.noreply.github.com>
2026-09-05 16:10:08 +0800

    feat: 修复图片物料多端云端双向同步，移除菜单选项内部对勾防止文字溢出，增加物料页下拉刷新

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "357373396",
+    serviceWorkerVersion: "1771239433",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
