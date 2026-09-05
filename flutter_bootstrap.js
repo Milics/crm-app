4710b584a2e5d9fb8dcaec3d44868cf2bc460f99
Milics <milics@users.noreply.github.com>
2026-09-05 15:50:15 +0800

    perf: 优化 iPhone Safari 启动体验，精简 canvaskit 瘦身 14MB，启用完整 ServiceWorker 离线秒开，增加平滑进度条

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: null,
+    serviceWorkerVersion: "357373396",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
