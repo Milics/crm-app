14480c4827752322890177a4f891cdc573a12dfd
Milics <milics@users.noreply.github.com>
2026-09-06 22:25:40 +0800

    feat: 发布已报名学员报名详情与回显修改版Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3301055100",
+    serviceWorkerVersion: "620791244",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
