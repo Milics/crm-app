bd88eaa6b6b3a8183e0da1d4eee0a1c353baa20d
Milics <milics@users.noreply.github.com>
2026-09-06 22:32:44 +0800

    style: 恢复顶部Tab字体大小支持横向左滑展示完整Tab，全部列表排除暂搁置

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "620791244",
+    serviceWorkerVersion: "2340763601",
   },
   config: {
     canvasKitBaseUrl: "canvaskit/",
